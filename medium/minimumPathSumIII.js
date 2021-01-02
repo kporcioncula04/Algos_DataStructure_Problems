7530f15ee0a5ea08443b7b52c4c78800d77f39c3
Karolina <kporcioncula.04@gmail.com>
2021-01-01 21:17:14 -0500

    add: med prob

---
+++ medium/minimumPathSumIII.js
@@ -0,0 +1,27 @@
+function  minPathSum(grid) {
+  let n = grid.length
+  let m = grid[0].length
+
+  for(let j = 1; j < m; j++){
+      grid[0][j]  +=  grid[0][j - 1]
+  }
+
+  for(let i = 1; i < n; i++){
+      grid[i][0] += grid[i-1][0]
+  }
+
+  for(let i = 1; i < n; i++){
+      for(let j = 1; j < m; j++){
+          grid[i][j] += Math.min(grid[i-1][j], grid[i][j - 1])
+      }
+  }
+
+  return grid[n - 1][m -1 ]
+}
+
+console.log(minPathSum([
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]))
+
